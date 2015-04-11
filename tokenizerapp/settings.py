d5ca6b778f766ab8512874b2cd80def50bb38d6d
Taranjeet <reachtotj@gmail.com>
2015-04-11 09:19:02 +0530

    mods for heroku

--- tokenizerapp/settings.py
+++ tokenizerapp/settings.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+ import dj_database_url
+#BASE_DIR = os.path.dirname(os.path.dirname(__file__)) #comment for heroku
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ADMINS = (
     ('Taranjeet Singh', 'reachtotj@gmail.com'),
   )
@@ -43,6 +45,9 @@ INSTALLED_APPS = (
     'categorizer',
     #'materialize',
 )
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -78,6 +83,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
