e71657ca76d9270dd6e004be9edcb5adbb40a0cf
Taranjeet <reachtotj@gmail.com>
2015-04-22 20:58:39 +0530

    index page refined

--- categorizer/views.py
+++ categorizer/views.py
@@ -326,6 +326,7 @@ def classify_news_score(input_news_dict):
 			p_international_sum+=log(input_news_dict[w]*p_word_international/p_word)
 	outcome=['business','entertainment','sports','national','international']
 	scores=[exp(p_business_sum+log(1.0/3.0)),exp(p_entertain_sum+log(1.0/3.0)),exp(p_sports_sum+log(1.0/3.0)),exp(p_national_sum+log(1.0/3.0)),exp(p_international_sum+log(1.0/3.0))]
+	print scores
 	max_score=max(scores)
 	message=outcome[scores.index(max_score)]
 	#print 'business = ', exp(p_business_sum+log(1.0/3.0)),'sports = ',exp(p_sports_sum+log(1.0/3.0)), 'entertain = ',exp(p_entertain_sum+log(1.0/3.0))
